48d37d10e1a062cf55989573a9b2daccf6a25298
Mikunj varsani <mikunj@live.com.au>
2016-02-18 16:49:49 +1100

    Added game listing

--- public/javascript/game/logic.js
+++ public/javascript/game/logic.js
@@ -1,13 +1,14 @@
 jQuery(document).ready(function($) {
     
+    //TODO Move this to another class
     var users = {};
+    var games = {};
     
     io = io.connect();
     
     //******** Click Events ********//
     
     $("#loginButton").click(function() {
-        console.log('bob');
         var nickname = $('#nameInput').val();
         if (nickname.length < 1) {
             $('#login .error').text('Invalid name');
@@ -15,6 +16,11 @@ jQuery(document).ready(function($) {
         } else {
             joinLobby(nickname);
         }
+    });
+    
+    $("#newGameButton").click(function() {
+        var name = prompt("Type in a title:", "bob");
+        io.emit($C.GAME.CREATE, { title: name });
     });
     
     //******** IO Events ********//
@@ -28,7 +34,14 @@ jQuery(document).ready(function($) {
                 users[user.id] = user;
             }
             
+            //Add the games to the list
+            for (var key in data.gameList) {
+                var game = data.gameList[key];
+                games[game.id] = game;
+            }
+            
             populateUserList();
+            populateGameList();
             
             $('#login').hide();
             
@@ -57,13 +70,23 @@ jQuery(document).ready(function($) {
         }
         populateUserList();
     });
-
-    io.on('gameCreated', function(data) {
-        console.log(data);
+    
+    io.on($C.GAME.CREATE, function(data) {
+        if (!data.hasOwnProperty('error')) {
+            console.log('Created game');
+        }
     });
-
-    io.on('gameRemoved', function(data) {
-        console.log(data);
+    
+    io.on($C.GAME.CREATED, function(data) {
+        games[data.id] = data;
+        populateGameList();
+    });
+          
+    io.on($C.GAME.REMOVED, function(data) {
+        if (games[data.id]) {
+            delete games[data.id];
+        }
+        populateGameList();
     });
     
     
@@ -77,7 +100,7 @@ jQuery(document).ready(function($) {
         $('#userList .content').empty();
         
         //Add user to the list
-        $.each(users, function(key, user) {
+        $.each(users, function(id, user) {
             var html = "<div class='user' data-id='" + user.id + "'>" + user.name + "</div>";
 
             //Check that we don't double up on adding users
@@ -86,11 +109,52 @@ jQuery(document).ready(function($) {
             }
         });
         
+        for (var i = 0; i < 50; i++) {
+            var html = "<div class='user' data-id='" + i + "'>" + i + "</div>";
+
+            //Check that we don't double up on adding users
+            if ($(".user[data-id='" + i + "']").length < 1) {
+                $('#userList .content').append(html);
+            }
+        }
+        
         //Set the user count
         $('#userList .top-bar').text('Connected Users ( ' + Object.keys(users).length + ' )');
     }
     
     var removeUserFromLobby = function(id) {
         $(".user[data-id='" + id + "']").remove();
+    }
+    
+    var populateGameList = function() {
+        $('#gameList .content').empty();
+        
+        //Add games to the list
+        $.each(games, function(id, game) {
+            var html = "<div class='game' data-id='" + game.id + "'>" +
+                        "<div id='title'>" + game.title + "</div>" +
+                        "<div id='status'>" + game.status + "</div>" +
+                        "<div id='players'>Players: " + game.players.length + "</div>" +
+                        "</div>"
+            
+            //Check that we don't double up on adding games
+            if ($(".game[data-id='" + game.id + "']").length < 1) {
+                $('#gameList .content').append(html);
+            }
+            
+        });
+        
+        for (var i = 0; i < 50; i++) {
+            var html = "<div class='game' data-id='" + i + "'>" +
+                        "<div id='title'>" + i + "</div>" +
+                        "<div id='status'>" + i + "</div>" +
+                        "<div id='players'>Players: " + i + "</div>" +
+                        "</div>"
+            
+            //Check that we don't double up on adding games
+            if ($(".game[data-id='" + i + "']").length < 1) {
+                $('#gameList .content').append(html);
+            }
+        }
     }
 });
